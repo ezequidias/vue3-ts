b981dcbdb00a4640b1d7f5bf61114fb14defe349
Ezequiel Dias <ezequidias@outlook.com>
2022-10-28 12:42:52 -0300

    Add toast and axios

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,25 @@
 /* eslint-disable import/order */
 import '@/@iconify/icons-bundle'
 import App from '@/App.vue'
+import axios from '@/plugins/axios'
 import vuetify from '@/plugins/vuetify'
 import { loadFonts } from '@/plugins/webfontloader'
 import router from '@/router'
 import '@/styles/styles.scss'
-import axios from '@/utils/request'
 import '@core/scss/index.scss'
 import { createPinia } from 'pinia'
 import { createApp } from 'vue'
+import Toast from "vue-toastification"
+import "vue-toastification/dist/index.css"
 
 loadFonts()
 
 const app = createApp(App)
-app.config.globalProperties.$axios = axios;
 
+app.use(Toast);
 app.use(vuetify)
-app.use(createPinia())
+app.use(axios)
+app.use(createPinia().use(({app, store}) => {store.$axios = axios.install(app)}))
 app.use(router)
 
 app.mount('#app')
