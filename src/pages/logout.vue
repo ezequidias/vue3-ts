a503ed3f6b04cf377e796b00ad1b1a248693ae6c
Ezequiel Dias <ezequidias@outlook.com>
2022-10-28 03:41:48 -0300

    Created page to logout user

---
+++ src/pages/logout.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores';
+import { useRouter } from 'vue-router';
+const router = useRouter()
+const authStore = useAuthStore();
+
+const handleLogout = async () => {
+  await authStore.logout()
+  router.push("/login");
+}
+
+onBeforeMount(async () => {
+  handleLogout()
+})
+</script>
+<template>
+</template>
+
