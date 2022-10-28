b0e2b50144ebb91354707f1d11d086e23e9e24a0
Ezequiel Dias <ezequidias@outlook.com>
2022-10-28 03:41:32 -0300

    Created auth in all pages

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,9 @@
-import { setupLayouts } from 'virtual:generated-layouts'
-import { createRouter, createWebHistory } from 'vue-router'
-import routes from '~pages'
+import { useAuthStore } from '@/stores';
+import { getToken } from '@/utils/AuthService';
+import axios from '@/utils/request';
+import { setupLayouts } from 'virtual:generated-layouts';
+import { createRouter, createWebHistory } from 'vue-router';
+import routes from '~pages';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,10 +15,27 @@ const router = createRouter({
   },
 })
 
-router.beforeEach((to, from) => {
-  // ...
-  // explicitly return false to cancel the navigation
-  return true
+router.beforeEach(async (to, from) => {
+   // redirect to login page if not logged in and trying to access a restricted page
+    const publicPages = ['/login', '/register'];
+    const authRequired = !publicPages.includes(to.path);
+    const auth = useAuthStore();
+    const token = getToken()
+
+    if(!auth.user && token){
+      try{
+        const user_details = await axios.get('user')
+        auth.status.loggedIn = true
+        auth.user = user_details.data
+        return '/'
+      }catch(e){
+        if(!authRequired) return true
+        return '/login'
+      }
+    }
+
+    if (authRequired && !auth.status.loggedIn) return '/login';
+    return true;
 })
 
 export default router
